92dd90f62ac532da67386ea0ef1d404f36c95f72
Branham <branham.mouata@gmail.com>
2026-07-14 14:57:00 +0200

    fix(sensor simulation): inline data buffer

--- lib/sensor_source/sensor_source.h
+++ lib/sensor_source/sensor_source.h
@@ -7,7 +7,7 @@
 inline constexpr std::size_t kSensorRawSampleCount = 1024U;
 inline constexpr std::size_t kSensorSampleRateHz = 48'000U;
 inline constexpr std::size_t kSensorPhaseSeconds = 5U;
-std::array<int16_t, kSensorRawSampleCount> sensor_buffer{};
+inline std::array<int16_t, kSensorRawSampleCount> sensor_buffer{};
 
 void sensor_source_next_frame(std::array<int16_t, kSensorRawSampleCount> &frame);
 std::size_t sensor_source_current_phase_index();
